11843bd94253736ebf61884cb0fc6627b68507c3
mjsabby <mjsabby@gmail.com>
2015-02-20 14:39:59 -0800

    Make LLVMSharp self-contained
    By including ClangSharp and ClangSharpPInvokeGenerator we're making
    LLVMSharp buildable in isolation.

---
+++ ClangSharpPInvokeGenerator/FunctionVisitor.cs
@@ -0,0 +1,53 @@
+﻿namespace ClangSharpPInvokeGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using ClangSharp;
+
+    internal sealed class FunctionVisitor : ICXCursorVisitor
+    {
+        private readonly TextWriter tw;
+
+        private readonly HashSet<string> visitedFunctions = new HashSet<string>();
+
+        private readonly string prefixStrip;
+
+        public FunctionVisitor(TextWriter tw, string libraryPath, string prefixStrip)
+        {
+            this.prefixStrip = prefixStrip;
+            this.tw = tw;
+            this.tw.WriteLine("        private const string libraryPath = \"" + libraryPath + "\";");
+            this.tw.WriteLine();
+        }
+
+        public CXChildVisitResult Visit(CXCursor cursor, CXCursor parent, IntPtr data)
+        {
+            if (cursor.IsInSystemHeader())
+            {
+                return CXChildVisitResult.CXChildVisit_Continue;
+            }
+
+            CXCursorKind curKind = clang.getCursorKind(cursor);
+
+            // look only at function decls
+            if (curKind == CXCursorKind.CXCursor_FunctionDecl)
+            {
+                var functionName = clang.getCursorSpelling(cursor).ToString();
+
+                if (this.visitedFunctions.Contains(functionName))
+                {
+                    return CXChildVisitResult.CXChildVisit_Continue;
+                }
+
+                this.visitedFunctions.Add(functionName);
+
+                Extensions.WriteFunctionInfoHelper(cursor, this.tw, this.prefixStrip);
+
+                return CXChildVisitResult.CXChildVisit_Continue;
+            }
+
+            return CXChildVisitResult.CXChildVisit_Recurse;
+        }
+    }
+}
