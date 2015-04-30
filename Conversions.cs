7c5524dd39c89dce554a0e44ade6d85c6179dad2
TChatzigiannakis <frozencompass@gmail.com>
2015-04-30 04:17:33 +0300

    Cleaner overloads for functions that take "out" parameters.

---
+++ Conversions.cs
@@ -0,0 +1,15 @@
+﻿namespace LLVMSharp
+{
+    partial struct LLVMBool
+    {
+        public static implicit operator bool(LLVMBool b)
+        {
+            return b.Value != 0;
+        }
+
+        public static implicit operator LLVMBool(bool b)
+        {
+            return new LLVMBool(b ? 1 : 0);
+        }
+    }
+}
