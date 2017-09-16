e16df0eb39ffb9c5151e2ef236ca887ef3ed2f07
Mukul Sabharwal <mjsabby@gmail.com>
2017-09-15 21:53:19 -0700

    Fix CDing.

--- tools/GenerateBindings.bat
+++ tools/GenerateBindings.bat
@@ -1,3 +1,5 @@
+cd ClangSharpPInvokeGenerator
+
 @echo off
 if [%1]==[] (
   echo **ERROR**: LLVM Shared Library Location is required. A good value for this parameter is 'llvm' which will translate to 'llvm.dll' or 'llvm.so' in deployment relative directory
@@ -10,4 +12,5 @@ if [%2]==[] (
 
 csc /out:ClangSharpPInvokeGenerator.exe ClangSharpPInvokeGenerator\*.cs
 ClangSharpPInvokeGenerator.exe --m LLVM --p LLVM --namespace LLVMSharp --output Generated.cs --libraryPath %1 --include %2 --file %2/llvm-c/Analysis.h --file %2/llvm-c/BitReader.h --file %2/llvm-c/BitWriter.h --file %2/llvm-c/Core.h --file %2/llvm-c/Disassembler.h --file %2/llvm-c/ErrorHandling.h --file %2/llvm-c/ExecutionEngine.h --file %2/llvm-c/Initialization.h --file %2/llvm-c/IRReader.h --file %2/llvm-c/Linker.h --file %2/llvm-c/LinkTimeOptimizer.h --file %2/llvm-c/lto.h --file %2/llvm-c/Object.h --file %2/llvm-c/OrcBindings.h --file %2/llvm-c/Support.h --file %2/llvm-c/Target.h --file %2/llvm-c/TargetMachine.h --file %2/llvm-c/Types.h --file %2/llvm-c/Transforms/IPO.h --file %2/llvm-c/Transforms/PassManagerBuilder.h --file %2/llvm-c/Transforms/Scalar.h --file %2/llvm-c/Transforms/Vectorize.h
-csc /target:library /out:LLVMSharp.dll ..\Conversions.cs ..\ExecutionEngine.cs Generated.cs ..\IRBuilder.cs ..\LLVMBasicBlockRef.cs ..\LLVMContextRef.cs ..\LLVMTypeRef.cs ..\LLVMValueRef.cs ..\Module.cs ..\Overloads.cs ..\PassManager.cs
+
+cd ..
--- tools/GenerateBindings.sh
+++ tools/GenerateBindings.sh
@@ -1,3 +1,5 @@
+cd ClangSharpPInvokeGenerator
+
 if [ -z "$1" ]; then
   echo **ERROR**: LLVM Shared Library Location is required. A good value for this parameter is 'libLLVM' which will translate to 'libLLVM.dll'/'libLLVM.so'/'libLLVM.dylib' on their respective platforms.
   exit 1
@@ -8,3 +10,5 @@ if [ -z "$2" ]; then
 fi
 
 dotnet run --m LLVM --p LLVM --namespace LLVMSharp --output Generated.cs --libraryPath $1 --include $2 --file $2/llvm-c/Analysis.h --file $2/llvm-c/BitReader.h --file $2/llvm-c/BitWriter.h --file $2/llvm-c/Core.h --file $2/llvm-c/Disassembler.h --file $2/llvm-c/ErrorHandling.h --file $2/llvm-c/ExecutionEngine.h --file $2/llvm-c/Initialization.h --file $2/llvm-c/IRReader.h --file $2/llvm-c/Linker.h --file $2/llvm-c/LinkTimeOptimizer.h --file $2/llvm-c/lto.h --file $2/llvm-c/Object.h --file $2/llvm-c/OrcBindings.h --file $2/llvm-c/Support.h --file $2/llvm-c/Target.h --file $2/llvm-c/TargetMachine.h --file $2/llvm-c/Types.h --file $2/llvm-c/Transforms/IPO.h --file $2/llvm-c/Transforms/PassManagerBuilder.h --file $2/llvm-c/Transforms/Scalar.h --file $2/llvm-c/Transforms/Vectorize.h
+
+cd ..
