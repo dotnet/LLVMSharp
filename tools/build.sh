a9f26347b637c552115300526960c2a009e4f182
Mukul Sabharwal <mjsabby@gmail.com>
2017-09-15 21:49:29 -0700

    cleanup tools dir.

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,5 @@
 if [ -z "$1" ]; then
-  echo **ERROR**: LLVM Shared Library Location is required. A good value for this parameter is 'llvm' which will translate to 'llvm.dll' or 'llvm.so' in deployment relative directory
+  echo **ERROR**: LLVM Shared Library Location is required. A good value for this parameter is 'libLLVM' which will translate to 'libLLVM.dll'/'libLLVM.so'/'libLLVM.dylib' on their respective platforms.
   exit 1
 fi
 if [ -z "$2" ]; then
@@ -7,6 +7,4 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-mcs /out:ClangSharpPInvokeGenerator.exe ClangSharpPInvokeGenerator/ClangSharp.Extensions.cs ClangSharpPInvokeGenerator/EnumVisitor.cs ClangSharpPInvokeGenerator/Extensions.cs ClangSharpPInvokeGenerator/ForwardDeclarationVisitor.cs ClangSharpPInvokeGenerator/FunctionVisitor.cs ClangSharpPInvokeGenerator/Generated.cs ClangSharpPInvokeGenerator/ICXCursorVisitor.cs ClangSharpPInvokeGenerator/Program.cs ClangSharpPInvokeGenerator/StructVisitor.cs ClangSharpPInvokeGenerator/TypeDefVisitor.cs
-mono ClangSharpPInvokeGenerator.exe --m LLVM --p LLVM --namespace LLVMSharp --output Generated.cs --libraryPath $1 --include $2 --file $2/llvm-c/Analysis.h --file $2/llvm-c/BitReader.h --file $2/llvm-c/BitWriter.h --file $2/llvm-c/Core.h --file $2/llvm-c/Disassembler.h --file $2/llvm-c/ExecutionEngine.h --file $2/llvm-c/Initialization.h --file $2/llvm-c/IRReader.h --file $2/llvm-c/Linker.h --file $2/llvm-c/LinkTimeOptimizer.h --file $2/llvm-c/lto.h --file $2/llvm-c/Object.h --file $2/llvm-c/Support.h --file $2/llvm-c/Target.h --file $2/llvm-c/TargetMachine.h --file $2/llvm-c/Transforms/IPO.h --file $2/llvm-c/Transforms/PassManagerBuilder.h --file $2/llvm-c/Transforms/Scalar.h --file $2/llvm-c/Transforms/Vectorize.h
-mcs /target:library /out:LLVMSharp.dll Conversions.cs ExecutionEngine.cs Generated.cs IRBuilder.cs LLVMBasicBlockRef.cs LLVMContextRef.cs LLVMTypeRef.cs LLVMValueRef.cs Module.cs Overloads.cs PassManager.cs
+dotnet run --m LLVM --p LLVM --namespace LLVMSharp --output Generated.cs --libraryPath $1 --include $2 --file $2/llvm-c/Analysis.h --file $2/llvm-c/BitReader.h --file $2/llvm-c/BitWriter.h --file $2/llvm-c/Core.h --file $2/llvm-c/Disassembler.h --file $2/llvm-c/ErrorHandling.h --file $2/llvm-c/ExecutionEngine.h --file $2/llvm-c/Initialization.h --file $2/llvm-c/IRReader.h --file $2/llvm-c/Linker.h --file $2/llvm-c/LinkTimeOptimizer.h --file $2/llvm-c/lto.h --file $2/llvm-c/Object.h --file $2/llvm-c/OrcBindings.h --file $2/llvm-c/Support.h --file $2/llvm-c/Target.h --file $2/llvm-c/TargetMachine.h --file $2/llvm-c/Types.h --file $2/llvm-c/Transforms/IPO.h --file $2/llvm-c/Transforms/PassManagerBuilder.h --file $2/llvm-c/Transforms/Scalar.h --file $2/llvm-c/Transforms/Vectorize.h
