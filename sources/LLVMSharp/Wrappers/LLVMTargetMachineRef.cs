f986236ebc9ae2b41584ad9b0fad96deadbbbe44
Avi Avni <avi.avni@gmail.com>
2019-08-19 19:53:07 +0300

    implement missing apis (#112)
    * implement missing apis
    
    * follow guidelines for TryEmitToFile, EmitToFile
    
    * call DisposeErrorMessage

--- sources/LLVMSharp/Wrappers/LLVMTargetMachineRef.cs
+++ sources/LLVMSharp/Wrappers/LLVMTargetMachineRef.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace LLVMSharp
 {
@@ -19,6 +20,50 @@ namespace LLVMSharp
         public static implicit operator LLVMOpaqueTargetMachine*(LLVMTargetMachineRef value)
         {
             return (LLVMOpaqueTargetMachine*)value.Pointer;
+        }
+
+        public string CreateTargetDataLayout()
+        {
+            var pDataLayout = LLVM.CreateTargetDataLayout(this);
+
+            if (pDataLayout is null)
+            {
+                return string.Empty;
+            }
+
+            var span = new ReadOnlySpan<byte>(pDataLayout, int.MaxValue);
+            return span.Slice(0, span.IndexOf((byte)'\0')).AsString();
+        }
+
+        public bool TryEmitToFile(LLVMModuleRef module, string fileName, LLVMCodeGenFileType codegen, out string message)
+        {
+            using (var marshaledFileName = new MarshaledString(fileName))
+            {
+                sbyte* errorMessage;
+
+                int result = LLVM.TargetMachineEmitToFile(this, module, marshaledFileName, codegen, &errorMessage);
+
+                if (errorMessage is null)
+                {
+                    message = string.Empty;
+                }
+                else
+                {
+                    var span = new ReadOnlySpan<byte>(errorMessage, int.MaxValue);
+                    message = span.Slice(0, span.IndexOf((byte)'\0')).AsString();
+                    LLVM.DisposeErrorMessage(errorMessage);
+                }
+
+                return result == 0;
+            }
+        }
+
+        public void EmitToFile(LLVMModuleRef module, string fileName, LLVMCodeGenFileType codegen)
+        {
+            if (!TryEmitToFile(module, fileName, codegen, out string Error))
+            {
+                throw new ExternalException(Error);
+            }
         }
     }
 }
