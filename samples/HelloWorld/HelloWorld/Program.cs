bab773e9982dd99866ce8fd021448a090b57d9f0
CWKSC <53114952+CWKSC@users.noreply.github.com>
2023-10-06 00:44:06 +0800

    Update Program.cs

--- samples/HelloWorld/HelloWorld/Program.cs
+++ samples/HelloWorld/HelloWorld/Program.cs
@@ -97,17 +97,16 @@ RunMain(module);
 /*
 [Output of LLVM IR]:
 
-;
-ModuleID = 'Hello World'
+; ModuleID = 'Hello World'
 source_filename = "Hello World"
 
-@0 = private unnamed_addr constant[13 x i8] c"Hello World!\00", align 1
+@0 = private unnamed_addr constant [13 x i8] c"Hello World!\00", align 1
 
 declare i32 @puts(ptr %0)
 
 define i32 @main() {
 entry:
-  % 0 = call i32 @puts(ptr @0)
+  %0 = call i32 @puts(ptr @0)
   ret i32 0
 }
 
