fb8f621699da07ed3244f75142c3cb37a7f49d2f
Tanner Gooding <tagoo@outlook.com>
2025-02-24 15:37:10 -0800

    Ensure properties expands to multiple options

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -19,7 +19,7 @@ $ErrorActionPreference = "Stop"
 
 function Build() {
   $logFile = Join-Path -Path $LogDir -ChildPath "$configuration\build.binlog"
-  & dotnet build -c "$configuration" --no-restore -v "$verbosity" /bl:"$logFile" /err "$properties" "$solution"
+  & dotnet build -c "$configuration" --no-restore -v "$verbosity" /bl:"$logFile" /err $properties "$solution"
 
   if ($LastExitCode -ne 0) {
     throw "'Build' failed for '$solution'"
@@ -55,7 +55,7 @@ function Help() {
 
 function Pack() {
   $logFile = Join-Path -Path $LogDir -ChildPath "$configuration\pack.binlog"
-  & dotnet pack -c "$configuration" --no-build --no-restore -v "$verbosity" /bl:"$logFile" /err "$properties" "$solution"
+  & dotnet pack -c "$configuration" --no-build --no-restore -v "$verbosity" /bl:"$logFile" /err $properties "$solution"
 
   if ($LastExitCode -ne 0) {
     throw "'Pack' failed for '$solution'"
@@ -64,7 +64,7 @@ function Pack() {
 
 function Restore() {
   $logFile = Join-Path -Path $LogDir -ChildPath "$configuration\restore.binlog"
-  & dotnet restore -v "$verbosity" /bl:"$logFile" /err "$properties" "$solution"
+  & dotnet restore -v "$verbosity" /bl:"$logFile" /err $properties "$solution"
 
   if ($LastExitCode -ne 0) {
     throw "'Restore' failed for '$solution'"
@@ -73,7 +73,7 @@ function Restore() {
 
 function Test() {
   $logFile = Join-Path -Path $LogDir -ChildPath "$configuration\test.binlog"
-  & dotnet test -c "$configuration" --no-build --no-restore -v "$verbosity" /bl:"$logFile" /err "$properties" "$solution"
+  & dotnet test -c "$configuration" --no-build --no-restore -v "$verbosity" /bl:"$logFile" /err $properties "$solution"
 
   if ($LastExitCode -ne 0) {
     throw "'Test' failed for '$solution'"
